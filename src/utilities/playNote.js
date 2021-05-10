34bccdb121b0962cb8169dff97ec4c65b7f9f7ab
elliothwang <elliothwang@gmail.com>
2021-05-10 14:30:34 -0700

    create playNote helper function

---
+++ src/utilities/playNote.js
@@ -0,0 +1,15 @@
+export default function playNote(note) {
+  if (note === "C") {
+  } else if (note === "C#") {
+  } else if (note === "D") {
+  } else if (note === "D#") {
+  } else if (note === "E") {
+  } else if (note === "F") {
+  } else if (note === "F#") {
+  } else if (note === "G") {
+  } else if (note === "G#") {
+  } else if (note === "A") {
+  } else if (note === "A#") {
+  } else if (note === "B") {
+  }
+}
