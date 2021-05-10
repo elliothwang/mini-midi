be47b820aa2264b2975ddfde3efe84e136f4ec97
elliothwang <elliothwang@gmail.com>
2021-05-10 14:24:24 -0700

    import Staff component to MainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,11 @@
+import Staff from "../../components/Staff/Staff";
 import Keyboard from "../../components/Keyboard/Keyboard";
 
 export default function MainPage () {
   return (
     <div className="mainPage">
       <h1>Main Page!</h1>
+      <Staff />
       <Keyboard />
     </div>
   )
