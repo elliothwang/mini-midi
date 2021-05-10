0c3e445a8968c71ac0b001bc0195563f639c047a
elliothwang <elliothwang@gmail.com>
2021-05-10 14:23:17 -0700

    add staff lines & trerble clef img

--- src/components/Staff/Staff.jsx
+++ src/components/Staff/Staff.jsx
@@ -1,7 +1,13 @@
+import "./Staff.css";
+
 export default function Keyboard() {
   return (
     <div className="staffContainer">
+      {/* <img src="https://toppng.com/uploads/preview/treble-clef-11552767864mvdst1kg5w.png" alt="treble clef" height="220vmin" className="trebleClefImg" /> */}
       <div className="staff">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
       </div>
     </div>
   );
