17f9264d457179d9afb52788acdfd38afa29b7ef
elliothwang <elliothwang@gmail.com>
2021-05-10 13:50:01 -0700

    create pages

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
 import './App.css';
 
@@ -16,13 +14,13 @@ export default function App() {
         <>
           <NavBar user={user} setUser={setUser} />
           <Switch>
-            <Route path="/orders/new">
-              <NewOrderPage />
+            <Route path="/">
+              
             </Route>
-            <Route path="/orders">
-              <OrderHistoryPage />
+            <Route path="/songs">
+
             </Route>
-            <Redirect to="/orders" />
+            <Redirect to="/" />
           </Switch>
         </>
         :
