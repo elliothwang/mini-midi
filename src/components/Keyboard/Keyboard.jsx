e3897b4ae7a3c6ed622bfce48d2ed28187e80184
elliothwang <elliothwang@gmail.com>
2021-05-10 14:17:25 -0700

    create basic key press functionality

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -12,6 +12,30 @@ export default function Keyboard({ notes, setNotes }) {
     // setNotes();
   }
 
+  function handleKeyDown(evt) {
+    // if keyCode= " ", invoke handleClick correctly
+    if (
+      evt.keyCode === 65 ||
+      evt.keyCode === 87 ||
+      evt.keyCode === 83 ||
+      evt.keyCode === 69 ||
+      evt.keyCode === 68 ||
+      evt.keyCode === 70 ||
+      evt.keyCode === 84 ||
+      evt.keyCode === 71 ||
+      evt.keyCode === 89 ||
+      evt.keyCode === 72 ||
+      evt.keyCode === 85 ||
+      evt.keyCode === 74
+    ) {
+      console.log(evt.key);
+    }
+  }
+
+    // capture keypress at the window/body level because non-input elements don't have an input focus
+    window.addEventListener("keydown", handleKeyDown);
+    // window.addEventListener("keyup", handleKeyUp);
+
   return (
     <div className="keyboardContainer">
       <div
