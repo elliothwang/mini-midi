5d0252eb4bae35fabf08e525df28f96852ef2bd7
elliothwang <elliothwang@gmail.com>
2021-05-10 22:31:50 -0700

    add state to Metronome component

--- src/components/Metronome/Metronome.jsx
+++ src/components/Metronome/Metronome.jsx
@@ -1,13 +1,21 @@
 import './Metronome.css';
+import { useState } from "react";
+import BPM120 from '../../assets/BPM/BPM120.mp3';
+
 
 export default function Metronome () {
-  let bpm = 120;
-  let playing = false;
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function playPauseMetronome () {
+    const BPM = new Audio(BPM120);
+    isPlaying ? BPM.pause() : BPM.play();
+    setIsPlaying(!isPlaying);
+  }
 
   return (
     <div className="metronome">
-      <div>{ bpm } BPM</div>
-      <button>{ playing ? 'Stop' : 'Start' }</button>
+      <div>120 BPM</div>
+      <button onClick={ playPauseMetronome }>{ isPlaying ? 'Pause' : 'Play' }</button>
     </div>
   )
 }
