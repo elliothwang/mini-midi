21c40d0a1d38cb7ae0b5b318758c1754bbf38e35
elliothwang <elliothwang@gmail.com>
2021-05-11 21:55:35 -0700

    display songs on MySongsPage

--- src/pages/MySongsPage/MySongsPage.jsx
+++ src/pages/MySongsPage/MySongsPage.jsx
@@ -1,7 +1,12 @@
-export default function MySongsPage () {
+export default function MySongsPage ({ userSongs }) {
   return (
     <div className="mySongsPage">
       <h1>My Songs Page!</h1>
+      <div className="mySongsContainer">
+        { userSongs.map((userSong, idx) => 
+          <div>Song { idx + 1}: { userSong }</div>
+          )}
+      </div>
     </div>
   )
 }
