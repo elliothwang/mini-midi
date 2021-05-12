01396ba24ffc36be28a86c1edf7ebec087cc5e68
elliothwang <elliothwang@gmail.com>
2021-05-12 16:42:25 -0700

    fix note img sizing

--- src/components/NoteImages/DownwardsQuarterNote/DownwardsQuarterNote.jsx
+++ src/components/NoteImages/DownwardsQuarterNote/DownwardsQuarterNote.jsx
@@ -3,7 +3,7 @@ import './DownwardsQuarterNote.css';
 export default function DownwardsQuarterNote () {
   return (
     <div className="downwardsQuarterNoteImg">
-      <img src="https://lh3.googleusercontent.com/proxy/tIdKRlIkkzQmHjrapTeZwHnt_xZ4b4bkOi5WL5HHwvkQTNil2mYUaH7mU8v6NCmoDsHp3bxlEiEFmEXXUmsYw5pC4UNIwJw" alt="downwards quarter note" height="35vmin" />
+      <img src="https://lh3.googleusercontent.com/proxy/tIdKRlIkkzQmHjrapTeZwHnt_xZ4b4bkOi5WL5HHwvkQTNil2mYUaH7mU8v6NCmoDsHp3bxlEiEFmEXXUmsYw5pC4UNIwJw" alt="downwards quarter note" />
     </div>
   )
 }
