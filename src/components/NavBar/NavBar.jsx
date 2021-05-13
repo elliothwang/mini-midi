73e4ef04dbf1b70b9c4d8c0472059fed1e7a5798
elliothwang <elliothwang@gmail.com>
2021-05-12 23:01:44 -0700

    add preliminary navbar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import "./NavBar.css";
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
 
@@ -9,13 +10,17 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
-      <Link to="/">Home</Link>
-      &nbsp; | &nbsp;
-      <Link to="/songs">My Songs</Link>
-      &nbsp; | &nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp; | &nbsp;
-      <Link to="" onClick={handleLogOut}>Log Out</Link>
+      <div className="welcome"><span>Welcome, {user.name}!</span></div>
+      <div className="dropdown">
+        <div className="dropdownIcon"><img 
+        src="https://toppng.com/uploads/preview/menu-icon-png-3-lines-11552739310fjzs2n2wxt.png"  
+        alt="dropdown menu icon" /></div>
+        <div className="dropdownContent">
+          <Link to="/">Home</Link>
+          <Link to="/songs">My Songs</Link>
+          <Link to="" onClick={handleLogOut}>Log Out</Link>
+        </div>
+      </div>
     </nav>
   );
 }
