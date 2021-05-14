d7d42b16944b2b2e24611c67f5cd76653424ec2d
elliothwang <elliothwang@gmail.com>
2021-05-13 20:01:53 -0700

    finish Navbar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ export default function NavBar({ user, setUser }) {
   }
 
   function openSideNav () {
-    document.querySelector(".sideNav").style.width = "200px";
+    document.querySelector(".sideNav").style.width = "225px";
   }
 
   function closeSideNav () {
@@ -18,9 +18,13 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
-      <div className="welcome"><span>welcome, <strong>{user.name}</strong>!</span></div>
+      <div className="welcome"><span>Welcome, <strong>{user.name}</strong>!</span></div>
       <div className="header">mini midi</div>
-        <div className="sideNavOpenButton" onClick={ openSideNav }><img src="https://toppng.com/uploads/preview/menu-icon-png-3-lines-11552739310fjzs2n2wxt.png" alt="dropdown menu icon" /></div>
+        <div className="sideNavOpenButton" onClick={ openSideNav }>
+          <div className="sideNavOpenButtonLine1"></div>
+          <div className="sideNavOpenButtonLine2"></div>
+          <div className="sideNavOpenButtonLine3"></div>
+        </div>
           <div className="sideNav">
             <Link to="/" onClick={ closeSideNav }>Home</Link>
             <Link to="/songs" onClick={ closeSideNav }>My Songs</Link>
