b707cf3e38d699db352b3aa7ffe5ad582409c33f
elliothwang <elliothwang@gmail.com>
2021-05-13 20:28:41 -0700

    finish MainPage

--- src/components/NoteImages/UpwardsQuarterNote/UpwardsQuarterNote.jsx
+++ src/components/NoteImages/UpwardsQuarterNote/UpwardsQuarterNote.jsx
@@ -3,7 +3,11 @@ import './UpwardsQuarterNote.css';
 export default function UpwardsQuarterNote () {
   return (
     <div className="upwardsQuarterNoteImg">
-      <img src="https://i.imgur.com/vsdGPSA.png" alt="upwards quarter note" height="20vmin" />
+      <img src="https://cdn1.iconfinder.com/data/icons/musical-notes-1/100/Music_Note2-01-512.png" alt="upwards quarter note" height="20vmin" />
     </div>
   )
 }
+
+// working links:
+// https://cdn1.iconfinder.com/data/icons/musical-notes-1/100/Music_Note2-01-512.png
+// https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Quarter_note_with_upwards_stem.svg/281px-Quarter_note_with_upwards_stem.svg.png
