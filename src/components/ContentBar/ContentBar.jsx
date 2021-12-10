50685213fc19d19312108e41389694d152a4316f
elliothwang <elliothwang@gmail.com>
2021-12-09 19:47:49 -0800

    refactor content bar

--- src/components/ContentBar/ContentBar.jsx
+++ src/components/ContentBar/ContentBar.jsx
@@ -3,6 +3,9 @@ import { useState, useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useInterval } from '../../hooks/useInterval';
 import BPM120 from '../../assets/BPM/BPM120.mp3';
+import { ReactComponent as Metronome } from '../../assets/Icons/stopwatch.svg';
+import { ReactComponent as Record } from '../../assets/Icons/record-circle.svg';
+
 // import * as songsAPI from "../../utilities/songs-api";
 
 export default function ContentBar({
@@ -67,18 +70,24 @@ export default function ContentBar({
           className={seconds ? 'record glowingRed' : 'record'}
           onClick={recordSong}
         >
-          {seconds ? seconds : completedRecording ? 'Save' : 'Record'}
+          {seconds ? (
+            seconds
+          ) : completedRecording ? (
+            'Save'
+          ) : (
+            <div>
+              <Record />
+            </div>
+          )}
         </button>
         <div className="metronome">
           <button
             className={isPlaying ? 'glowingGreen' : ''}
             onClick={playPauseMetronome}
           >
-            {isPlaying ? 'Pause' : 'Play'}
-            <img
-              src="https://img.icons8.com/ios/452/metronome.png"
-              alt="metronome"
-            />
+            <div>
+              <Metronome />
+            </div>
           </button>
         </div>
         <div className={note ? 'noteTrackerSelected' : 'noteTrackerUnselected'}>
